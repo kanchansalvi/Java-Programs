d934555a5ecb01f4760c25bd0cb71df3a6a6348e
kanchansalvi <deepkanchan.sonawane@gmail.com>
2022-02-02 19:16:57 -0800

    Updated

--- Basics/KeyWords.java
+++ Basics/KeyWords.java
@@ -1,5 +1,102 @@
 public class KeyWords {
+
+    public static class Table {
+       enum Type  {
+            KITCHEN,
+            DINING;
+        };
+    };
+
+    public enum Season { WINTER, SPRING, SUMMER, FALL } 
+    
     public static void main(String args[]){  
         System.out.println("Hello Java");  
+        int age = 6;
+        // if(age<18) {
+        //     throw new ArithmeticException("Person is not eligible to vote");
+        // }
+
+        //continue
+        //Loop restarts from the continue
+        for(int i=0; i<8; i++){
+
+            if(i%2==0)continue;
+
+            System.out.print(" : "+i);
+        }
+
+        int arr[] = new int[5];
+        // for
+        for(int i1=0; i1<arr.length; i1++){
+            arr[i1] = i1*2;
+        }
+        // what is extended for loop?
+        for(int i1 : arr){
+            System.out.print(" : "+i1);
+        }
+
+
+        // new - for creating instance
+
+        // enum- 
+        // when to use it
+        // sample enum
+
+    for (Season s : Season.values()){  
+        System.out.println(s);  
+    }  
+    System.out.println("Value of WINTER is: "+Season.valueOf("WINTER"));  
+    System.out.println("Index of WINTER is: "+Season.valueOf("WINTER").ordinal());  // place of enum starts at 0
+
+
+        // switch
+        // write switch for int/char/string values
+        // extra - write switch with enums
+        int option = 2;
+        switch (option) {
+            case 1:
+                System.out.println("Yes");
+                break;
+            case 2:
+                System.out.println("No");  //Outputs "No"
+                break;
+            default:        
+                System.out.println("Undecided");
+         }
+
+
+
+        
+
+
+
     }
 }
+
+/*
+abstract	continue	for	        new	        switch
+assert***	default	    goto*	    package	    synchronized
+boolean	    do	        if	        private	    this
+break	    double	    implements	protected	throw
+byte	    else	    import	    public	    throws
+case	    enum****	instanceof	return	    transient
+catch	    extends	    int	        short	    try
+char	    final	    interface	static	    void
+class	    finally	    long	    strictfp**	volatile
+const*	    float	    native	    super	    while
+
+*/
+
+/* - Ganesh changed
+abstract(later)	continue	for	        new	        switch
+assert***	default	    goto*	    package	    synchronized
+boolean	    do	        if	        private	    this
+break	    double	    implements	protected	throw
+byte	    else	    import	    public	    throws
+case	    enum****	instanceof	return	    transient
+catch	    extends	    int	        short	    try
+char	    final	    interface	static	    void
+class	    finally	    long	    strictfp**	volatile
+const*	    float	    native	    super	    while
+
+*/
--- Basics/Operators.java
+++ Basics/Operators.java
@@ -21,5 +21,57 @@ public class Operators {
         }
         System.out.println("After pre increment the values are : i="+i+" m1="+m1);
 
+
+
+    //Unary Operators
+    int x=10;  
+    System.out.println(x++);//10 (11)  
+    System.out.println(++x);//12  
+    System.out.println(x--);//12 (11)  
+    System.out.println(--x);//10  
+    int a=10;  
+    int b=-10;  
+    System.out.println(~a);//-11 (minus of total positive value which starts from 0)  
+    System.out.println(~b);//9 (positive of total minus, positive starts from 0) 
+    
+    //Left Shift Operator
+    System.out.println(10<<2);//10*2^2=10*4=40  
+    System.out.println(10<<3);//10*2^3=10*8=80  
+    System.out.println(20<<2);//20*2^2=20*4=80  
+    System.out.println(15<<4);//15*2^4=15*16=240  
+
+    //Right Shift Operator
+    System.out.println(10>>2);//10/2^2=10/4=2  
+    System.out.println(20>>2);//20/2^2=20/4=5  
+    System.out.println(20>>3);//20/2^3=20/8=2 
+
+    //Shift Operator Example: >> vs >>>
+     //For positive number, >> and >>> works same  
+     System.out.println(20>>2);  
+     System.out.println(20>>>2);  
+     //For negative number, >>> changes parity bit (MSB) to 0  
+     System.out.println(-20>>2);  
+     System.out.println(-20>>>2);  
+
+     //Logical && and Bitwise &
+     int c=20;  
+    System.out.println(a<b&&a<c);//false && true = false  
+    System.out.println(a<b&a<c);//false & true = false  
+
+    //Logical || and Bitwise |
+    System.out.println(a>b||a<c);//true || true = true  
+    System.out.println(a>b|a<c);//true | true = true  
+    //|| vs |  
+    System.out.println(a>b||a++<c);//true || true = true  
+    System.out.println(a);//10 because second condition is not checked  
+    System.out.println(a>b|a++<c);//true | true = true  
+    System.out.println(a);//11 because second condition is checked  
+
+    //Ternary Operator
+
+    int min=(a<b)?a:b;  
+    System.out.println(min);  
+
+
        }  
 }
